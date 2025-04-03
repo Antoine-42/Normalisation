4b50fc357a82e7857093818ce1efaa73f06b5cd4
Antoine-42 <antoinegraeffz@gmail.com>
2025-04-03 11:46:42 +0200

    Delete evale_srv_moyenne.c directory

--- evale_srv_moyenne.c/evale_srv_moyenne.h
+++
@@ -1,9 +0,0 @@
-#define TAILLE 8
-#define RESTE 4
-void supprimer(int tab[], int *taille, int index);
-int trouverMax(int tab[], int taille);
-int trouverMin(int tab[], int taille);
-int lireValeurs(int tab[]);
-void afficherValeurs(float tab[], int taille);
-float calculerMoyenne(float tab[]);
-void filtrerEtConvertir(int original[], float resultat[]);
